f1cde6bd6c9f7dd911d2ee916a9693fdc9f08447
Sky Resendez <sky.resendez@gmail.com>
2018-09-04 02:47:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,8 +15,8 @@ class TicTacToe
       user_input.to_i - 1
     end
 
-    def move(@board, index, current_player)
-      board[index] = current_player
+    def move(index, current_player)
+      @board[index] = current_player
     end
 
     def valid_move?(board, pos)
